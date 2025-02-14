e0b68e9afce93cf1262f3c9d4b81612d943e9c05
Mubeen Maqsood <mubeenmaqsood2003@gmail.com>
2025-02-13 22:10:09 -0500

    Delete CSC_311_Module 3 Homework directory

--- CSC_311_Module 3 Homework/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module edu.farmingdale.csc_311_module_3_homework {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens edu.farmingdale.csc_311_module_3_homework to javafx.fxml;
-    exports edu.farmingdale.csc_311_module_3_homework;
-}
